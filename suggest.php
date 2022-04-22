60f6ae620491ac408b6ebaa3f9a76196b0a0279b
Yanhao Jin <yanhao.jin@outlook.com>
2022-04-22 10:58:52 -0700

    [hw5] part2 multi words suggestion

--- suggest.php
+++ suggest.php
@@ -3,24 +3,33 @@
 require_once('Apache/Solr/Service.php');
 $solr = new Apache_Solr_Service('localhost', 8983, '/solr/myexample');
 $query = "";
+$previous = "";
+$final_query_term = "";
 if(!empty($_GET["q"])) {
     $query = $_GET["q"];
+    $pos = strrpos($query, " ");
+    if ($pos === false) {
+        $final_query_term = $query;
+    } else {
+        $final_query_term = substr($query, $pos + 1);
+        $previous = substr($query, 0 , $pos)." ";
+    }
 }
 
 $limit = 10;
-$result = $solr->autoCompleteQuery($query);
+$result = $solr->autoCompleteQuery($final_query_term);
 
 if(!empty($result)) {
     $raw = $result->__get("suggest");
     $raw= json_decode( json_encode($raw), true);
-    $res_arr = $raw["suggest"][$query]["suggestions"];
+    $res_arr = $raw["suggest"][$final_query_term]["suggestions"];
 ?>
     
     <ul id="suggestion-list">
     <?php
     foreach($res_arr as $term) {
     ?>
-    <li onClick="selectCountry('<?php echo $term["term"]; ?>');"><?php echo $term["term"]; ?></li>
+    <li onClick="selectCountry('<?php echo $previous . $term["term"]; ?>');"><?php echo  $previous . $term["term"]; ?></li>
     <?php } ?>
     </ul>
     <?php } ?>
